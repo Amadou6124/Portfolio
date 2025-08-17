f44734abc748b00f28a15b96e73f5b91603d57c8
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-17 22:06:14 +0000

    Mise en place de django admin

--- src/portfolio_app/admin.py
+++ src/portfolio_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Certificat, Competences
 
 # Register your models here.
+
+@admin.register(Competences)
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+
+@admin.register(Certificat)
+class CertificatAdmin(admin.ModelAdmin):
+    list_display = ['titre']
