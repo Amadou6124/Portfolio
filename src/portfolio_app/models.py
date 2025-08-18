9fd3b9b9ec1d3b60e24310c5a47dd02b476f4b26
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-18 00:32:20 +0000

    Ajout du model de contact

--- src/portfolio_app/models.py
+++ src/portfolio_app/models.py
@@ -35,3 +35,13 @@ class Projet(models.Model):
     
     def __str__(self):
         return self.titre
+    
+class Contact(models.Model):
+    nom = models.CharField(max_length=100)
+    email = models.EmailField()
+    sujet = models.CharField(max_length=200)
+    message = models.TextField()
+    date_envoi = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message de {self.nom} - {self.sujet}"
