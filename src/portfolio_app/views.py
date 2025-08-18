486d3b2caaffe14b53227b920c414011722add30
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-18 00:39:01 +0000

    Mise en place du formulaire de contact avec ModelForm et son vues

--- src/portfolio_app/views.py
+++ src/portfolio_app/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Competences, Certificat, Projet
+from .forms import ContactForm
+
 
 # Create your views here.
 def portfolio_onepage(request):
     competences = Competences.objects.all()
     certificats = Certificat.objects.all()
     projets = Projet.objects.all()
-    return render(request, 'portfolio_app/index.html', context={'competences': competences, 'certificats': certificats, 'projets': projets })
+
+    # La logique du formulaire de contact
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accueil') # Redirige vers la page d'accueil après envoi
+    else:
+        form = ContactForm()
+    return render(request, 'portfolio_app/index.html', context={'competences': competences, 'certificats': certificats, 'projets': projets, 'form': form })
