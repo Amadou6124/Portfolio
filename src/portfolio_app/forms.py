87b144649a30886b6299b6a00ab70e972a43c4cb
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-18 00:52:40 +0000

    Ajout des widgets css dans forms.py mise a jour du templates du page contact et du style css

--- src/portfolio_app/forms.py
+++ src/portfolio_app/forms.py
@@ -2,6 +2,22 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+
+    # Ajoute les classes CSS aux widgets
+    nom = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    sujet = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Contact
         fields = ['nom', 'email', 'sujet', 'message']
+
